84353ddf8d071bda55eec6ebd2de399516a3aeb2
DivineCrusade <gorlovdima@inbox.ru>
2022-07-26 23:54:05 +0300

    style: Стилизация верхней части основного блока
    Стилизация первых трех блоков после хедера, подключение фреймворка для слайдеров.
    
    - Стилизовать блок преимуществ.
    - Добавить промо-видео.
    - Стилизовать блок акций.
    - Добавить библиотеку JQuery и Slick фреймворк (для слайдера).
    - Добавить слайдер с акциями и стилизовать его.
    - Добавить иконки для слайдера.
    - Добавить простую анимацию для слайдов акций.
    - Стилизовать блок с рассрочкой (за исключением чекбокса).
    - Доработать сетку первых трех блоков после хедера.

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.slider-promo').slick({
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1,
+        initialSlide: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        dots: true
+    })
+})
