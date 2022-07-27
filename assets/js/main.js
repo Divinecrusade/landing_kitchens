13e407353d5671e73e656f2400e5a74182eeb4e6
DivineCrusade <gorlovdima@inbox.ru>
2022-07-27 16:59:32 +0300

    feat: Интеграция блока "Материалы"
    Блок взят с другого лендинга компании и стилизован согласно макету. Также немного поправлена верстка.
    
    - Интегрировать блок "Материалы" с прошлого лендинга фабрики.
    - Убрать лишнее из верстки блока.
    - Стилизовать блок по требованиям дизайна.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-$(function() {
+jQuery(document).ready(function() {
+//-------------------------------------------------------------------
+
+    
+    // Слайдеры
+    
 	$('.slider-promo').slick({
         centerMode: true,
         centerPadding: '120px',
@@ -7,7 +12,7 @@ $(function() {
         adaptiveHeight: true,
         arrows: true,
         dots: true
-    })
+    });
     
     $('.slider-manufacture').slick({
         centerMode: true,
@@ -19,5 +24,41 @@ $(function() {
         dots: true,
         autoplay: true,
         autoplaySpeed: 4000
-    })
+    });
+//-------------------------------------------------------------------
+    
+    
+    // Материалы
+    
+    $('input[name="radio"]').click(function(){
+        var target = $('#type-materials-' + $(this).val());
+        let value = $(this).val();
+        $('.type-materials').not(target).hide(0);
+        target.fadeIn(500);
+        if (value == 1){
+            $( "#kitch_Type_1_1" ).trigger( "click" );    
+        }
+        else if (value == 2){
+            $( "#kitch_Type_2_1" ).trigger( "click" );  
+        }
+        else if (value == 3){
+            $( "#kitch_Type_3_1" ).trigger( "click" );
+        }    
+    });
+    
+
+    $('input[name="radioSec"]').click(function(){   
+                
+            var target2 = $('#desc_text-' + $(this).val()); 
+            
+            $('.desc_text').not(target).hide(0);
+            target2.fadeIn(500);     
+    });
+    var target = $('#type-materials-1');
+    $('.type-materials').hide(0);
+    target.fadeIn(500);
+    var target2 = $('#desc_text-1');
+    $('.desc_text').hide(0);
+    target2.fadeIn(500);
+//-------------------------------------------------------------------
 })
