fa298a6e00b0b2cc33493ae62d70db0f7935abb3
DivineCrusade <gorlovdima@inbox.ru>
2022-07-27 19:30:57 +0300

    feat: Меню
    Добавлено всплывающее меню.
    
    - Разметить и стилизовать меню, изначально скрытое.
    - Написать js-код, реализующий сокрытие и показ меню.
    - Разместить на странице якоря для меню.

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,4 +63,44 @@ jQuery(document).ready(function() {
     $('.desc_text').hide(0);
     target2.fadeIn(500);
 //-------------------------------------------------------------------
+    
+    
+    // Меню
+    
+    let flag = true;
+    let menu = $('#menu');
+    menu.hide();
+    let basis;
+
+    $('#headerToggleMenu, #footerToggleMenu, #menu a').click(function(){
+        if (flag){
+            basis = window.pageYOffset || document.documentElement.scrollTop;
+            
+            menu.fadeIn(500);
+            flag=false;
+        }
+        else{            
+            menu.hide(500);
+            flag=true;
+        }
+    });
+    
+    $(document).mouseup( function(e){
+        if (!menu.is(e.target) && menu.has(e.target).length === 0 && !flag && !$('#headerToggleMenu, #footerToggleMenu, #menu a').is(e.target) && $('#headerToggleMenu, #footerToggleMenu, #menu a').has(e.target).length === 0) 
+        {
+            menu.hide(500);
+            flag=true;
+        }
+    });
+    
+    window.onscroll = function () {
+        if (!flag) {
+            var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+            if (Math.abs(basis - scrolled) > 400) {
+                menu.hide(500);
+                flag = true;
+            }
+        }
+    };
+//-------------------------------------------------------------------
 })
