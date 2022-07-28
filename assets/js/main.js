4f31a8ba36c67917426485f0f4e5b395583e531f
DivineCrusade <gorlovdima@inbox.ru>
2022-07-28 19:43:35 +0300

    style: Адаптивная стилизация
    Используя сетку Bootstrap и медиа-запросов проведена адаптация лендинга к экранам длиной вплоть до 576 px.
    
    - Переверстать и перестилизовать слайдер акции.
    - Переверстать и перестилизовать блок контакты.
    - Добавить Bootstrap классы для адаптации блоков к разным расширениям экрана (до 576 px).
    - Добавить медиа-запросы, адаптирующие контент страницы к разным расширениям (до 576 px).

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,18 @@ jQuery(document).ready(function() {
         adaptiveHeight: true,
         arrows: true,
         dots: true,
-        speed: 750
+        speed: 750,
+        mobileFirst: true/*,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        }]*/
     });
     
     $('.slider-manufacture').slick({
@@ -25,7 +36,14 @@ jQuery(document).ready(function() {
         dots: true,
         autoplay: true,
         speed: 1000,
-        autoplaySpeed: 4000
+        autoplaySpeed: 4000,
+        responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            centerPadding: '100px',
+          }
+        }]
     });
 //-------------------------------------------------------------------
     
