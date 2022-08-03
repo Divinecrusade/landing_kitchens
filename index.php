7bd04bbcd3e6d9a9b1783fa08cd1c34b8f0e60df
DivineCrusade <gorlovdima@inbox.ru>
2022-08-03 19:12:39 +0300

    refactor: Минификация
    Добавление префиксов и минификация.
    
    - Добавить префиксы для правил в файле style.css.
    - Минифицировать js- и css-файлы.

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 		
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/style.min.css">
 		
 		<title>Кухни-Слон | Дизайн кухни онлайн</title>
         <style>            
@@ -1566,7 +1566,7 @@
         <script src="<?=ASSETS_PATH?>assets/lib/jquery/jquery-3.6.0.min.js"></script>
         <script src="<?=ASSETS_PATH?>assets/lib/lazysizes/lazysizes.min.js"></script>
         <script src="<?=ASSETS_PATH?>assets/lib/slick/slick.min.js"></script>        
-        <script src="<?=ASSETS_PATH?>assets/js/main.js"></script>
+        <script src="<?=ASSETS_PATH?>assets/js/main.min.js"></script>
     
         <link rel="stylesheet" href="<?=ASSETS_PATH?>assets/lib/slick/slick.css">
         <link rel="stylesheet" href="<?=ASSETS_PATH?>assets/lib/slick/slick-theme.css">
