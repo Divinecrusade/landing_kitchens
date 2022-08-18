e465da085340b740dc461d4b30fb0b28b8feb4b4
DivineCrusade <gorlovdima@inbox.ru>
2022-08-18 15:26:26 +0300

    feat: Добавление виджета чата с дизайнером

--- index.php
+++ index.php
@@ -1527,5 +1527,13 @@
                 language: 'ru'
             });
         </script>
+        
+        <!-- Виджет с чатом -->
+        <script>
+            (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+            })(window,document,'https://cdn-ru.bitrix24.ru/b18733946/crm/site_button/loader_2_j5tvx2.js');
+        </script>
 	</body>
 </html>
